38294e974911c062d9c9a59ebb959a7657e2bcb4
nokkun <at.mc1.18@gmail.com>
2020-05-06 20:52:00 +0900

    tkinterを導入してGUIを少しいじってみた。 TreeViewが使えそう。

--- play_ground.py
+++ play_ground.py
@@ -1,74 +1,25 @@
-# 自由にコードで遊ぶためのプレイグラウンド
+import tkinter as tk
 
-import os
-import pandas as pd
+root = tk.Tk()
+root.title("Thinter Test")
+root.geometry("400x300")
 
-# l = ['ben','car','apple','ben','car','dick']
-#
-# # 順番はランダムで、リストの重複を取り除く
-# print(set(l))
-# print(list(set(l)))
-# #{'car', 'apple', 'ben', 'dick'}
-# #['car', 'apple', 'ben', 'dick']
-#
-# # 順序を保持したまま、リストの重複を取り除く
-# print((dict.fromkeys(l)))
-# # リストにする
-# print((list(dict.fromkeys(l))))
-# #{'ben': None, 'car': None, 'apple': None, 'dick': None}
-# #['ben', 'car', 'apple', 'dick']
-#
-#
-# def my_index(l, x, default=False):
-#     if x in l:
-#         return l.index(x)
-#     else:
-#         return default
-#
-# print(my_index(l,"apple"))
-# # 2
-# print(my_index(l,"car"))
-# # 1
-# print(my_index(l,"oppai"))
-# # False
+def push1():
+    textBox.delete(0, tk.END)
+    textBox.insert(tk.END, "push1")
+def push2():
+  print("push2")
+def push3():
+  print("push3")
 
-query = "legpress"
-file_name = "hoge.pkl"
+btn1 = tk.Button(root, text="Button1", width="20",command=push1)
+btn1.place(x=10, y=10)
+btn2 = tk.Button(root, text="Button2", width="20", command=push2)
+btn2.place(x=10, y=40)
+btn3 = tk.Button(root, text="Button3", width="20", command=push3)
+btn3.place(x=10, y=70)
 
-print("---start---:::", query)
+textBox = tk.Entry(root)
+textBox.place(x=200, y=10)
 
-if os.path.exists(file_name):
-    print(pd.read_pickle(file_name))
-
-# 初回検索の場合
-if not os.path.exists(file_name):
-    # Create
-    print("Create")
-    df = pd.DataFrame({'ST': [query],
-                       'RankA': 2,
-                       'RankB': 3
-                       }
-                      )
-    df.to_pickle(file_name)
-else:
-    df = pd.read_pickle(file_name)
-
-    if df[df["ST"] == query].empty:
-        # Add
-        print("Add")
-        new_df = pd.DataFrame({'ST': [query],
-                           'RankA': 12,
-                           'RankB': 33
-                           }
-                          )
-        df.append(new_df, ignore_index=True).to_pickle(file_name)
-    else:
-        # Update
-        print("Update")
-        index = df[df["ST"] == query].index
-        df.iloc[index, 1] = 2
-        df.iloc[index, 2] = -1
-        df.to_pickle(file_name)
-
-print("---finish---")
-print(pd.read_pickle(file_name))
+root.mainloop()
