4e3706b2079758ae4bfaeee618d8a475bb39a953
nokkun <at.mc1.18@gmail.com>
2020-05-08 21:59:00 +0900

    検索アルゴリズムを大幅に変更。サイト内のページはカウントしないようにした。 GUIは、時間とヒット数のカラムを追加、サイトURLを入力できるようにした。

--- check_ranking.py
+++ check_ranking.py
@@ -1,96 +1,42 @@
-import os
-from urllib.parse import urlparse
 from googlesearch import search
 import pandas as pd
+import os
+import datetime
 
 
-# リストの中から自分のブログのドメインのインデックスを返す。
-# 見つからなかった場合にはFalseを返す
-def my_index(l, x, default=False):
-    if x in l:
-        return l.index(x)
-    else:
-        return default
-
-
-def calc_ranking_by_query(query, your_site_domain):
-
-    ranking_dict = {}
-    ranking_dict['SearchTerm'] = query
+def search_term(query, target_site_domain, file_name):
 
     ## 日本語のGoogle検索を行う。言語は日本語。１ページあたりの検索数は10個,50個検索して止める。
     search_result_list = list(search(query, lang="jp", num=10, stop=50, pause=1))
+    df = pd.DataFrame(search_result_list, columns=["urls"])
 
-    domain_list = []
-    for index, url in enumerate(search_result_list):
-        # URLのランキングを辞書に登録する
-        if your_site_domain in url:
-            # より上位の順位を登録する、最初に登場したドメインが含まれたURLを登録する
-            if "Ranking(URL)" not in ranking_dict.keys():
-                ranking_dict["Ranking(URL)"] = index + 1
-        else:
-            # 見つからなかった場合には-1を登録する
-            ranking_dict["Ranking(URL)"] = -1
+    ## 重複を消す
+    clean_df = df[(~df.urls.str.contains("#"))].reset_index()
 
-        # URLからドメインを抽出してリストに格納する
-        parsed_uri = urlparse(url)
-        domain = '{uri.scheme}://{uri.netloc}'.format(uri=parsed_uri)
-        domain_list.append(domain)
+    ## ドメインを検索
+    index = clean_df[clean_df.urls.str.contains(target_site_domain)].index.to_list()
 
-    # ドメインの重複を無くす
-    unique_domain_list = list(dict.fromkeys(domain_list))
+    ## インデックスに＋１をしてランキングにする
+    rank = [n + 1 for n in index]
 
-    # サイトのドメインが見つかった場合は辞書に登録する
-    if my_index(unique_domain_list, your_site_domain) is not False:
-        ranking = my_index(unique_domain_list, your_site_domain) + 1
-        ranking_dict['Ranking(Domain)'] = ranking
-    # サイトのドメインが見つからなかった場合には-1を辞書に登録する
-    else:
-        ranking_dict['Ranking(Domain)'] = -1
+    dt_now = datetime.datetime.now()
 
-    return ranking_dict
+    data = pd.DataFrame({'SearchTerm': query,
+                         'Ranking': [rank],
+                         'Total': len(rank),
+                         'TargetSite': target_site_domain,
+                         'Date': dt_now
+                         }
+                        )
 
-
-def add_update_pickle(query, your_site_domain, file_name):
-    print("---start---:::", query)
-
-    if os.path.exists(file_name):
-        print(pd.read_pickle(file_name))
-
-    # 初回検索の場合
     if not os.path.exists(file_name):
-        # データフレームを新しく作って保存する
-        print("Create")
-        ranking_dict = calc_ranking_by_query(query, your_site_domain)
-        df = pd.DataFrame({'SearchTerm': [query],
-                           'Ranking(Domain)': ranking_dict['Ranking(Domain)'],
-                           'Ranking(URL)': ranking_dict['Ranking(URL)']
-                           }
-                          )
-        df.to_pickle(file_name)
+        df = pd.DataFrame()
     else:
         df = pd.read_pickle(file_name)
-        # 該当する検索ワードが無かったら新しくデータフレームを作って追加する
-        if df[df["SearchTerm"] == query].empty:
-            # Add
-            print("Add")
-            ranking_dict = calc_ranking_by_query(query, your_site_domain)
-            df2 = pd.DataFrame({'SearchTerm': [query],
-                                'Ranking(Domain)': ranking_dict['Ranking(Domain)'],
-                                'Ranking(URL)': ranking_dict['Ranking(URL)']
-                                }
-                               )
-            df.append(df2, ignore_index=True).to_pickle(file_name)
-        else:
-            # 該当する検索ワードがあったらランキングを更新する
-            # Update
-            print("Update")
-            index = df[df["SearchTerm"] == query].index
-            ranking_dict = calc_ranking_by_query(query, your_site_domain)
 
-            df.loc[index, 'Ranking(Domain)'] = ranking_dict['Ranking(Domain)']
-            df.loc[index, 'Ranking(URL)'] = ranking_dict['Ranking(URL)']
-            df.to_pickle(file_name)
+    df = df.append(data)
+    df.reset_index(drop=True, inplace=True)
+    print(df)
+    df.to_pickle(file_name)
 
-    print("---finish---")
-    print(pd.read_pickle(file_name))
+    return
--- gui.py
+++ gui.py
@@ -3,42 +3,43 @@ import tkinter.ttk as ttk
 import pandas as pd
 import check_ranking
 
-your_site_domain = 'https://ymgsapo.com'
-
-# 保存したデータフレームの読み込み
 file_name = "ranking.pkl"
-df = pd.read_pickle(file_name)
 
 # ウインドウの作成
 base = tk.Tk()
 base.title("RankChecker")
-base.geometry("400x300")
+base.geometry("700x300")
 
-# テキストボックスの設定
-EditBox = tk.Entry(width=20)
-EditBox.insert(tk.END, "最初に表示される文字列")
-EditBox.pack()
+# テキストボックス(query)の設定
+EditBox_query = tk.Entry(width=20)
+EditBox_query.insert(tk.END, "最初に表示される文字列")
+EditBox_query.pack()
+
+# テキストボックス(ドメイン)の設定
+EditBox_url = tk.Entry(width=20)
+EditBox_url.insert(tk.END, "domain")
+EditBox_url.pack()
 
 # ツリービューの設定
 tree = ttk.Treeview(base)
 
 # 列を３列作る
-tree["column"] = (1, 2, 3)
+tree["column"] = (1, 2, 3, 4, 5)
 tree["show"] = "headings"
 
 # ヘッダーテキスト
 tree.heading(1, text="検索ワード")
-tree.heading(2, text="ランキング(ドメイン)")
-tree.heading(3, text="ランキング（URL）")
+tree.heading(2, text="ランキング")
+tree.heading(3, text="数")
+tree.heading(4, text="ドメイン")
+tree.heading(5, text="時間")
 
 # 列の幅
 tree.column(1, width=200)
-tree.column(2, width=100)
-tree.column(3, width=100)
-
-# データ挿入
-for i in range(df.index.stop):
-    tree.insert("", "end", values=(df['SearchTerm'][i], df['Ranking(Domain)'][i], df['Ranking(URL)'][i]))
+tree.column(2, width=50)
+tree.column(3, width=50)
+tree.column(4, width=200)
+tree.column(5, width=200)
 
 # ツリービューの設置
 tree.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
@@ -50,26 +51,31 @@ def refresh():
         tree.delete(i)
 
     # 保存したデータフレームの読み込み
-    df2 = pd.read_pickle(file_name)
+    df = pd.read_pickle(file_name)
 
     # 全データ挿入
-    for i in range(df2.index.stop):
-        tree.insert("", "end", values=(df2['SearchTerm'][i], df2['Ranking(Domain)'][i], df2['Ranking(URL)'][i]))
+    for i in range(df.index.stop):
+        tree.insert("", "end", values=(df['SearchTerm'][i], df['Ranking'][i], df['Total'][i],
+                                       df['TargetSite'][i], df['Date'][i]))
 
 
 # clickイベント
 def query_click():
-    # エントリーに入力された内容を取得
-    query = EditBox.get()
+    # ドメインを取得
+    your_site_domain = EditBox_url.get()
+    # クエリーに入力された内容を取得
+    query = EditBox_query.get()
     # ランキングを検索
-    check_ranking.add_update_pickle(query, your_site_domain, file_name)
+    check_ranking.search_term(query, your_site_domain, file_name)
     # ツリービューの初期化
     refresh()
 
 
 # ボタン
 btn = tk.Button(base, text='検索', command=query_click)
-btn.place(x=300, y=0)
+btn.place(x=500, y=0)
+
+refresh()
 
 # GUIアプリの実行
 base.mainloop()
