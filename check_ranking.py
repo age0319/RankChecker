1440837ab27a3bad49ed370f58dec8037ff8f238
nokkun <at.mc1.18@gmail.com>
2020-05-09 09:36:50 +0900

    検索用クラスとGUIクラスを作成。 PycharmとJupyterで同じ動作ができるようにした。 （Pycharmが本番、Jupyterはテスト環境:gui_test.ipynb） TreeViewの選択行を取得できるようにした。 ドメイン用のテキストボックスを削除。

--- check_ranking.py
+++ check_ranking.py
@@ -1,42 +1,47 @@
 from googlesearch import search
-import pandas as pd
 import os
 import datetime
+import pandas as pd
 
+class Search():
+    def __init__(self, query, domain, filename):
+        self.query = query
+        self.domain = domain
+        self.filename = filename
 
-def search_term(query, target_site_domain, file_name):
+    def search_term(self):
 
-    ## 日本語のGoogle検索を行う。言語は日本語。１ページあたりの検索数は10個,50個検索して止める。
-    search_result_list = list(search(query, lang="jp", num=10, stop=50, pause=1))
-    df = pd.DataFrame(search_result_list, columns=["urls"])
+        ## 日本語のGoogle検索を行う。言語は日本語。１ページあたりの検索数は10個,50個検索して止める。
+        search_result_list = list(search(self.query, lang="jp", num=10, stop=50, pause=1))
+        df = pd.DataFrame(search_result_list, columns=["urls"])
 
-    ## 重複を消す
-    clean_df = df[(~df.urls.str.contains("#"))].reset_index()
+        ## 重複を消す
+        clean_df = df[(~df.urls.str.contains("#"))].reset_index()
 
-    ## ドメインを検索
-    index = clean_df[clean_df.urls.str.contains(target_site_domain)].index.to_list()
+        ## ドメインを検索
+        index = clean_df[clean_df.urls.str.contains(self.domain)].index.to_list()
 
-    ## インデックスに＋１をしてランキングにする
-    rank = [n + 1 for n in index]
+        ## インデックスに＋１をしてランキングにする
+        rank = [n + 1 for n in index]
 
-    dt_now = datetime.datetime.now()
+        dt_now = datetime.datetime.now()
 
-    data = pd.DataFrame({'SearchTerm': query,
-                         'Ranking': [rank],
-                         'Total': len(rank),
-                         'TargetSite': target_site_domain,
-                         'Date': dt_now
-                         }
-                        )
+        data = pd.DataFrame({'SearchTerm': self.query,
+                             'Ranking': [rank],
+                             'Total': len(rank),
+                             'TargetSite': self.domain,
+                             'Date': dt_now
+                             }
+                            )
 
-    if not os.path.exists(file_name):
-        df = pd.DataFrame()
-    else:
-        df = pd.read_pickle(file_name)
+        if not os.path.exists(self.filename):
+            df = pd.DataFrame()
+        else:
+            df = pd.read_pickle(self.filename)
 
-    df = df.append(data)
-    df.reset_index(drop=True, inplace=True)
-    print(df)
-    df.to_pickle(file_name)
+        df = df.append(data)
+        df.reset_index(drop=True, inplace=True)
+        print(df)
+        df.to_pickle(self.filename)
 
-    return
+        return
--- gui.py
+++ gui.py
@@ -1,81 +1,94 @@
 import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import ttk
 import pandas as pd
-import check_ranking
-
-file_name = "ranking.pkl"
-
-# ウインドウの作成
-base = tk.Tk()
-base.title("RankChecker")
-base.geometry("700x300")
-
-# テキストボックス(query)の設定
-EditBox_query = tk.Entry(width=20)
-EditBox_query.insert(tk.END, "最初に表示される文字列")
-EditBox_query.pack()
-
-# テキストボックス(ドメイン)の設定
-EditBox_url = tk.Entry(width=20)
-EditBox_url.insert(tk.END, "domain")
-EditBox_url.pack()
-
-# ツリービューの設定
-tree = ttk.Treeview(base)
-
-# 列を３列作る
-tree["column"] = (1, 2, 3, 4, 5)
-tree["show"] = "headings"
-
-# ヘッダーテキスト
-tree.heading(1, text="検索ワード")
-tree.heading(2, text="ランキング")
-tree.heading(3, text="数")
-tree.heading(4, text="ドメイン")
-tree.heading(5, text="時間")
-
-# 列の幅
-tree.column(1, width=200)
-tree.column(2, width=50)
-tree.column(3, width=50)
-tree.column(4, width=200)
-tree.column(5, width=200)
-
-# ツリービューの設置
-tree.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+from check_ranking import Search
 
 
-def refresh():
-    # ツリービューの初期化
-    for i in tree.get_children():
-        tree.delete(i)
+class RankCheckerGUI:
+    def __init__(self):
+        self.filename = "ranking.pkl"
+        self.domain = "ymgsapo.com"
 
-    # 保存したデータフレームの読み込み
-    df = pd.read_pickle(file_name)
+        self.base = tk.Tk()
+        self.base.title("RankChecker")
+        self.base.geometry("800x800")
 
-    # 全データ挿入
-    for i in range(df.index.stop):
-        tree.insert("", "end", values=(df['SearchTerm'][i], df['Ranking'][i], df['Total'][i],
-                                       df['TargetSite'][i], df['Date'][i]))
+        # テキストボックス
+        self.entryBox = tk.Entry(master=self.base)
+        self.entryBox.pack()
+
+        # ボタン
+        self.button1 = tk.Button(
+            master=self.base,
+            text="Button",  # 初期値
+            width=15,  # 幅
+            bg="lightblue",  # 色
+            command=self.query_click  # クリックに実行する関数
+        )
+        self.button1.pack()
+
+        # TreeViewの設定
+        self.tree = ttk.Treeview(self.base)
+
+        # 列を作る
+        self.tree["column"] = (1, 2, 3, 4, 5, 6)
+        self.tree["show"] = "headings"
+
+        # ヘッダーテキスト
+        self.tree.heading(1, text="No.")
+        self.tree.heading(2, text="検索ワード")
+        self.tree.heading(3, text="ランキング")
+        self.tree.heading(4, text="数")
+        self.tree.heading(5, text="ドメイン")
+        self.tree.heading(6, text="時間")
+
+        # 列の幅
+        self.tree.column(1, width=10)
+        self.tree.column(2, width=100)
+        self.tree.column(3, width=10)
+        self.tree.column(4, width=10)
+        self.tree.column(5, width=50)
+        self.tree.column(6, width=50)
+
+        # ツリービューの設置
+        self.tree.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        # 選択されたTreeviewを取得するための関数をバインド
+        self.tree.bind("<<TreeviewSelect>>", self.on_tree_select)
+
+        self.load_from_pkl()
+
+        self.base.mainloop()
+
+    def on_tree_select(self, event):
+        print("selected items:")
+        for item in self.tree.selection():
+            item_text = self.tree.item(item, "values")
+            print(item_text[0])
+
+    def query_click(self):
+        # 検索語を取得
+        query = self.entryBox.get()
+
+        # ランキングを検索
+        sr = Search(query, self.domain, self.filename)
+        sr.search_term()
+
+        # ツリービューの削除
+        for i in self.tree.get_children():
+            self.tree.delete(i)
+
+        self.load_from_pkl()
+
+    def load_from_pkl(self):
+        # 保存したデータフレームの読み込み
+        self.df = pd.read_pickle(self.filename)
+
+        # 全データ挿入
+        for i in range(self.df.index.stop):
+            self.tree.insert("", "end", values=(i, self.df['SearchTerm'][i], self.df['Ranking'][i], self.df['Total'][i],
+                                                self.df['TargetSite'][i], self.df['Date'][i]))
 
 
-# clickイベント
-def query_click():
-    # ドメインを取得
-    your_site_domain = EditBox_url.get()
-    # クエリーに入力された内容を取得
-    query = EditBox_query.get()
-    # ランキングを検索
-    check_ranking.search_term(query, your_site_domain, file_name)
-    # ツリービューの初期化
-    refresh()
-
-
-# ボタン
-btn = tk.Button(base, text='検索', command=query_click)
-btn.place(x=500, y=0)
-
-refresh()
-
-# GUIアプリの実行
-base.mainloop()
+if __name__ == "__main__":
+    app = RankCheckerGUI()
